2c406686537dfbde70316dc252cd6e8c23f23496
parvg555 <parvg555@gmail.com>
2022-04-10 02:47:58 +0530

    made a simple box and used texture

--- js/main.js
+++ js/main.js
@@ -14,9 +14,43 @@ renderer.setSize(
 
 document.body.appendChild(renderer.domElement);
 
+// making window dynamic
+window.addEventListener('resize', function() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    renderer.setSize(width,height);
+    camera.aspect = width/height;
+    camera.updateProjectionMatrix();
+});
+
+// enabling orbit controls 
+controls = new THREE.OrbitControls(camera,renderer.domElement);
+
+//creating a shape
+var geometry = new THREE.BoxGeometry(1,1,1);
+var cubeMatrial = new THREE.MeshPhongMaterial({
+    map: new THREE.TextureLoader().load('../assets/textures/cubeTexture.png'),
+    color: 0xFFFFFF,
+    wireframe: false
+})
+
+// create a material, color or image texture
+var material = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    wireframe: false,
+    map: new THREE.TextureLoader().load("../assets/textures/cubeTexture.png")
+});
+
+var cube = new THREE.Mesh(geometry,material);
+
+scene.add(cube)
+
+camera.position.z = 3;
+
 // updates every frame
 var update = function () {
-
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.005;
 };
 
 // draws scene
